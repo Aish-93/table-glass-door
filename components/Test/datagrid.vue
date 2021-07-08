3d369def2aedcab90b4a3a277eb880581a14f077
Aishwary Saxena <aish2saxena@gmail.com>
2021-07-08 15:21:24 +0530

    methods adding

--- components/Test/datagrid.vue
+++ components/Test/datagrid.vue
@@ -1,35 +1,41 @@
 <template>
   <v-container>
-    <v-row class="d-flex flex-column col-12 ">
-      <div class="mx-auto"><b>Table-Project </b></div>
-      <thead class="spacers">
+    <v-row class="d-flex flex-column justify-content-center">
+      <div class="mt-5 text-center"><b>Table-Project </b></div>
+      <div class="best" height="400px">
+      <thead  >
         <tr >
-          <th class="spacers"> Block</th>
-       
           <th class="spacers">
+            <input type="checkbox" value="checked" id="mycheck" @click="marker" :checked="bean">
+             Block</th>
+       
+          <th class="spacers" @click="sortId" >
             #
           </th>
 
-          <th class="spacers">Name</th>
+          <th class="spacers" @click="sorting">Name</th>
 
           <th class="spacers">Email</th>
 
           <th class="spacers">Role(s)</th>
- class="spacers"
+ 
           <th class="spacers">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(temp, index) in datas" :key="index">
-          <td><button>icon</button></td>
-          <td>{{ temp.id }}</td>
-          <td>{{ temp.Name }}}</td>
-          <td>{{ temp.email }}</td>
-          <td>{{ temp.role }}</td>
-          <td>icon</td>
+        <tr v-for="(temp, index) in datas" :key="index" class="spacers" @click="marker" :class="bean.length > 0 ? 'pick' : '' ">
+          <td class="spacers " @click="picker(index)" :class="dream.length > 0 ? 'pick' :'' ">
+            <input type="checkbox" id="mybox" value="checked"  :checked="dream" ></td>
+
+           
+          <td class="spacers">{{ temp.id }}</td>
+          <td class="spacers">{{ temp.Name }}</td>
+          <td class="spacers">{{ temp.email }}</td>
+          <td class="spacers">{{ temp.role }}</td>
+          <td class="spacers ">icon</td>
         </tr>
       </tbody>
-    
+    </div>
     </v-row>
   </v-container>
 </template>
@@ -39,18 +45,32 @@
     box-sizing: border-box;
     margin:0px;
     padding: 0px;
+   
+}
+.pick{
+  background-color: rgb(228, 202, 202);
 }
 
 .spacers{
     margin:15px;
     padding: 2px 10px 2px 10px ;
+    min-width: 180px;
+    text-align: center;
+    border:2px solid orange;
+    
+
 }
 
 </style>
 <script>
+// import bootstrap from 'bootstrap';
 export default {
   data: function() {
     return {
+      striker:false,
+      bean:'',
+      kill:9,
+      dream:'',
       datas: [
         {
           id: 1,
@@ -65,31 +85,71 @@ export default {
           role: "Admin Editor Viewer"
         },
         {
-          id: 3,
+          id: 4,
           Name: "Jim",
           email: "jim@abc.com",
           role: "Viewer"
         },
         {
-          id: 4,
+          id: 3,
           Name: "Sam",
           email: "sam@abc.com",
           role: " Editor Viewer"
         },
         {
-          id: 5,
+          id: 6,
           Name: "Larry",
           email: "larry@abc.com",
           role: " Viewer"
         },
         {
-          id: 6,
+          id: 5,
           Name: "Monica",
           email: "monica@abc.com",
           role: " Editor Viewer"
         }
       ]
     };
+  },
+  methods:{
+    sorting() {
+      this.datas.sort(function (a,b){
+        var nameA = a.Name.toUpperCase();
+        var nameB = b.Name.toUpperCase();
+
+         if (nameA < nameB) {
+    return -1;
+      }
+  if (nameA > nameB) {
+    return 1;
+  }
+      })
+      },
+
+      sortId(){
+       this.datas.sort(function (a,b){
+         return a.id - b.id ;
+       })
+      },
+
+      marker(){
+        this.striker = !this.striker;
+        console.log(this.striker);
+        if(this.striker === true ){
+          this.bean = 'checked';
+        }else {
+          this.bean ='';
+        }
+
+      },
+      picker(index){
+        this.kill = this.index;
+        if(this.kill = this.index){
+          this.dream = "checked"
+        }
+        console.log(this.kill)
+
+      }
   }
 };
 </script>
