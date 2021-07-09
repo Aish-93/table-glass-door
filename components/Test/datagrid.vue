99bc13a6fe77140cf7d873e9b99978b7b5444ddc
Paresh Chouhan <paresh@cumul.io>
2021-07-09 19:19:08 +0530

    newmethods

--- components/Test/datagrid.vue
+++ components/Test/datagrid.vue
@@ -1,4 +1,4 @@
-<template>
+                                                                                                                                                      `<template>
   <v-container>
     <v-row class="d-flex flex-column justify-content-center">
       <div class="mt-5 text-center"><b>Table-Project </b></div>
@@ -23,16 +23,17 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(temp, index) in datas" :key="index" class="spacers" @click="picker(index)"
-        :class="bean.length > 0 ? 'pick' : '' ">
+        <tr v-for="(temp, rowIndex) in datas" :key="rowIndex" class="spacers" 
+        :class="{ 'pick': bean.length > 0 , 'ring': select[rowIndex] } ">
           <td class="spacers " >
-            <input type="checkbox" id="mybox" value="checked"  :checked="dream" ></td>
+            <input type="checkbox" id="mybox" :value="select[rowIndex]"
+              @click="myFilter(rowIndex)"  :checked="bean"></td>
 
            
-          <td class="spacers">{{ temp.id }}</td>
-          <td class="spacers">{{ temp.Name }}</td>
-          <td class="spacers">{{ temp.email }}</td>
-          <td class="spacers">{{ temp.role }}</td>
+          <td class="spacers" :class="{ active: rowIndex === selected.x && 0 === selected.y}">{{ temp.id }}</td>
+          <td class="spacers" :class="{ active: rowIndex === selected.x && 1 === selected.y}">{{ temp.Name }}</td>
+          <td class="spacers" :class="{ active: rowIndex === selected.x && 2 === selected.y}">{{ temp.email }}</td>
+          <td class="spacers" :class="{ active: rowIndex === selected.x && 3 === selected.y}">{{ temp.role }}</td>
           <td class="spacers ">icon</td>
         </tr>
       </tbody>
@@ -51,6 +52,12 @@
 .pick{
   background-color: rgb(228, 202, 202);
 }
+.ring{
+   background-color: rgb(228, 202, 202);
+}
+.active {
+  border: 2px solid blue !important;
+}
 
 .spacers{
     margin:15px;
@@ -64,6 +71,8 @@
 
 </style>
 <script>
+// import Direction from 'vue-direction-key'
+// Vue.use(Direction)
 // import bootstrap from 'bootstrap';
 export default {
   data: function() {
@@ -71,7 +80,12 @@ export default {
       striker:false,
       bean:'',
       kill:[],
-      dream:'',
+      dream:false,
+      select:[],
+      selected: {
+        x: 0,
+        y: 0
+      },
       datas: [
         {
           id: 1,
@@ -148,9 +162,79 @@ export default {
         console.log(this.kill);
 
       },
-      singleMarker(){
-        
+      myFilter(index){
+        // this.$set(this.select, index, !this.select[index])//for only vue
+        this.select = this.select.map((selectedItem, _index) => _index === index ? !selectedItem : selectedItem );
+        // this.select[index] = !this.select[index];
+        console.log(this.select);        
       },
-  }
+      rows(){
+        for(let i = 0; i < this.datas.length; i++){
+          this.select.push(false);
+          console.log(this.select);
+        }
+      },
+      created: function () {
+  // let direction = this.$getDirection()
+  
+}
+  },
+  mounted: function() {
+    this.rows()
+    document.addEventListener('keydown', (e) => {
+      console.log('jere', e);
+      if (e.key === 'ArrowRight') {
+        console.log('here')
+        if (this.selected.y === 3){
+          this.selected=  {
+            x : this.selected.x,
+            y: 3
+          }
+        }else{ 
+        this.selected = {
+            x : this.selected.x,
+            y: this.selected.y + 1
+          };
+        }
+
+       
+      }
+      if (e.key === 'ArrowLeft') {
+        if(this.selected.y === 0){
+          this.selected = {
+            x: this.selected.x,
+            y: 0
+          }
+        }else{
+        this.selected ={
+         x: this.selected.x,
+
+        y: this.selected.y - 1
+        }
+        }
+      }
+      if (e.key === 'ArrowUp') {
+        if(this.selected.x === 0){
+          this.selected={
+            x: 0,
+            y: this.selected.y
+          }
+          
+        }else{
+          this.selected= {
+            x:this.selected.x - 1,
+            y:this.selected.y
+          }
+        }
+      }
+      if (e.key === 'ArrowDown') {
+        if(this.selected.x === this.datas.length){
+          this.selected.x = this.datas.length
+        }else{
+        this.selected.x = this.selected.x + 1;}
+      }
+    })
+  },
+  
 };
 </script>
